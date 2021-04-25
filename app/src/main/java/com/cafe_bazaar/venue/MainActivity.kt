5c6cb377ec60a577bab387b61763b3ad16585baa
mahmoud mirghafari <mahmoud.mirghafari@gmail.com>
2021-04-25 08:12:41 +0430

    init commit

---
+++ app/src/main/java/com/cafe_bazaar/venue/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cafe_bazaar.venue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
